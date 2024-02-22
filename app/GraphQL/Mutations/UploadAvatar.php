f1ec6fe5e197e02ca2dc416d4f4ec0703669383e
Alphius <alphiuswambua@gmail.com>
2024-02-22 18:36:39 +0300

    Setup mutations,queries, auth and file upload

---
+++ app/GraphQL/Mutations/UploadAvatar.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+
+final readonly class UploadAvatar
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        $file = $args['image'];
+        $path = $file->storePublicly('public/uploads');
+        $user = User::find($args['id']);
+        $user->update(['avatar' => $path]);
+        return $user;
+    }
+}
